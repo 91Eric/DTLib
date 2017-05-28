eabb4dd0d8ac51f569a34787c781d8467665e399
Eric <849931616@qq.com>
2017-05-28 13:48:57 +0800

    add comment for some file

--- include/SmartPointer.h
+++ include/SmartPointer.h
@@ -13,7 +13,7 @@ public:
     {
         m_pointer = p;
     }
-    SmartPointer(const SmartPointer<T>& obj)
+    SmartPointer(const SmartPointer<T>& obj)  //kaobeigouzao chushi m_pointer jiushi null buxuyao delete duibi operator=
     {
         m_pointer=obj.m_pointer;
          //此处是为了保证堆空间只被一个智能指针指向，因此原对象指针要置NULL,首先类型转换，因为参数是const不能修改
@@ -24,10 +24,12 @@ public:
     {
         if(this!=&obj)
         {
-            delete m_pointer;
+            T* tmp = this->m_pointer;
+              //此处是否应该先保存，毕竟如果是类对象的会触发析构函数，有可能有异常，后续待测试!!!!!!!!
             m_pointer=obj.m_pointer;
              //此处是为了保证堆空间只被一个智能指针指向，因此原对象指针qingkong,首先类型转换，因为参数是const不能修改
             //注意！！ 转换的参数仍然是SmartPointer<T>并且是引用
+            delete tmp;
             const_cast<SmartPointer<T>&>(obj).m_pointer=NULL;
         }
         return *this;
--- include/exception.h
+++ include/exception.h
@@ -4,7 +4,9 @@
 namespace DTLib
 {
 
-class Exception
+#define THROW_EXCEPTION(e,m) (throw e(m,__FILE__,__LINE__))
+
+class Exception   //顶层异常的抽象基类
 {
 public:
     Exception(const char* message);
@@ -17,7 +19,7 @@ public:
     virtual const char* message() const;
     virtual const char* location() const;
 
-    virtual ~Exception()=0;
+    virtual ~Exception()=0;   //纯虚函数被实现了，就变为虚函数，C++规定，所有析构函数必须有实现！！！！
 protected:
     char* m_message;
     char* m_location;
@@ -26,15 +28,15 @@ protected:
 
 };
 
-class ArithmeticExxception:public Exception
+class ArithmeticExxception:public Exception //计算异常
 {
 public:
-    ArithmeticExxception():Exception(0){}
+    ArithmeticExxception():Exception(0){}  //显式调用父类构造函数 Exception(const char* message),其中message 为NULL ,这样分配的m_message实际是NULL
     ArithmeticExxception(const char* message):Exception(message){}
     ArithmeticExxception(const char* file,int line):Exception(file,line){}
     ArithmeticExxception(const char* message,const char* file,int line):Exception(message,file,line){}
 
-    ArithmeticExxception(const ArithmeticExxception& e):Exception(e){}
+    ArithmeticExxception(const ArithmeticExxception& e):Exception(e){}   //拷贝构造函数直接调用父类的构造函数
 
     ArithmeticExxception& operator=(const ArithmeticExxception& e)
     {
@@ -43,7 +45,7 @@ public:
     }
 };
 
-class IndexOutOfBoundsExceptio:public Exception
+class IndexOutOfBoundsExceptio:public Exception  //越界
 {
 public:
     IndexOutOfBoundsExceptio():Exception(0) {}
@@ -58,6 +60,41 @@ public:
         Exception::operator =(e);
         return *this;
     }
+};
+
+class NullPointerException:public Exception  //空指针
+{
+public:
+    NullPointerException():Exception(0){}
+    NullPointerException(const char* message):Exception(message){}
+    NullPointerException(const char* file,int line):Exception(file,line){}
+    NullPointerException(const char* message,const char* file,int line):Exception(message,file,line){}
+
+    NullPointerException(const NullPointerException& e):Exception(e){}
+
+    NullPointerException& operator =(const NullPointerException& e)
+    {
+        Exception::operator =(e);
+        return *this;
+    }
+
+};
+
+class NoEnoughMemoryException:public Exception  //内存不足
+{
+public:
+    NoEnoughMemoryException():Exception(0){}
+    NoEnoughMemoryException(const char* message):Exception(message){}
+    NoEnoughMemoryException(const char* file,int line):Exception(file,line){}
+    NoEnoughMemoryException(const char* message,const char* file,int line):Exception(message,file,line){}
+
+    NoEnoughMemoryException(const NoEnoughMemoryException& e):Exception(e){}
+
+
+};
+class InvalidParameterException:public Exception  //参数错误
+{
+
 };
 
 }
